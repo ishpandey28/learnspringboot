1672d3713b4aa0d85b88915b62ff851cae149fdc
Ish Pandey <28ishpandey@gmail.com>
2024-04-24 11:22:19 +0530

    first commit

---
+++ src/main/java/com/ish/springbootbasics/SpringbootbasicsApplication.java
@@ -0,0 +1,13 @@
+package com.ish.springbootbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootbasicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ish/springbootbasics/SpringbootbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ish.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
