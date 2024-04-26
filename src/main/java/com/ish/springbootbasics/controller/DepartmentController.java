cd9f6465c9e578e166e66cd29acec984b3e0caa0
Ish Pandey <28ishpandey@gmail.com>
2024-04-26 12:48:19 +0530

    third day

--- src/main/java/com/ish/springbootbasics/controller/DepartmentController.java
+++ src/main/java/com/ish/springbootbasics/controller/DepartmentController.java
@@ -1,8 +1,12 @@
 package com.ish.springbootbasics.controller;
 
 import com.ish.springbootbasics.entity.Department;
+import com.ish.springbootbasics.error.DepartmentNotFoundException;
 import com.ish.springbootbasics.repository.DepartmentRepository;
 import com.ish.springbootbasics.service.DepartmentService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,8 +17,11 @@ public class DepartmentController {
     @Autowired
     private DepartmentService departmentService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(DepartmentController.class);
+
     @PostMapping("/departments")
-    public Department saveDepartment(@RequestBody Department department){
+    public Department saveDepartment(@Valid @RequestBody Department department){
+        LOGGER.info("inside save Department of deparment controller");
         return departmentService.saveDepartment(department);
     }
 
@@ -24,7 +31,23 @@ public class DepartmentController {
     }
 
     @GetMapping("/departments/{id}")
-    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId) throws DepartmentNotFoundException {
+        LOGGER.info("inside fetch Department of deparment controller");
         return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id") Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "department deleted successfully";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartmentById(@PathVariable("id") Long departmentId,@RequestBody Department department){
+        return departmentService.updateDepartmentById(departmentId, department);
+    }
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentbyName(@PathVariable("name") String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
     }
 }
--- src/main/java/com/ish/springbootbasics/service/DepartmentServiceImpl.java
+++ src/main/java/com/ish/springbootbasics/service/DepartmentServiceImpl.java
@@ -1,11 +1,14 @@
 package com.ish.springbootbasics.service;
 
 import com.ish.springbootbasics.entity.Department;
+import com.ish.springbootbasics.error.DepartmentNotFoundException;
 import com.ish.springbootbasics.repository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class DepartmentServiceImpl implements DepartmentService{
@@ -23,8 +26,41 @@ public class DepartmentServiceImpl implements DepartmentService{
     }
 
     @Override
-    public Department fetchDepartmentById(Long departmentID) {
-        return departmentRepository.findById(departmentID).get();
+    public Department fetchDepartmentById(Long departmentID) throws DepartmentNotFoundException {
+
+        Optional<Department> department= departmentRepository.findById(departmentID);
+        if(!department.isPresent()){
+            throw new DepartmentNotFoundException("Department not available");
+        }
+
+        return department.get();
+    }
+
+    @Override
+    public void deleteDepartmentById(Long departmentId) {
+        departmentRepository.deleteById(departmentId);
+    }
+
+    @Override
+    public Department updateDepartmentById(Long departmentId, Department department) {
+        Department depDB = departmentRepository.findById(departmentId).get();
+
+        if(Objects.nonNull(department.getDepartmentName()) && !"".equalsIgnoreCase(department.getDepartmentName())){
+            depDB.setDepartmentName(department.getDepartmentName());
+        }
+        if(Objects.nonNull(department.getDepartmentCode()) && !"".equalsIgnoreCase(department.getDepartmentCode())){
+            depDB.setDepartmentCode(department.getDepartmentCode());
+        }
+        if(Objects.nonNull(department.getDepartmentAddress()) && !"".equalsIgnoreCase(department.getDepartmentAddress())){
+            depDB.setDepartmentAddress(department.getDepartmentAddress());
+        }
+
+        return departmentRepository.save(depDB);
+    }
+
+    @Override
+    public Department fetchDepartmentByName(String departmentName) {
+        return departmentRepository.findByDepartmentNameIgnoreCase(departmentName);
     }
 
 
